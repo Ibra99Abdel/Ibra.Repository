3011f6463322c6721fc95cc1e9ca775561974a5d
Ibra99Abdel <149332950+Ibra99Abdel@users.noreply.github.com>
2023-11-05 13:40:52 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shape;
+
+/**
+ *
+ * @author MY LAPTOP
+ */
+public class Circle {
+    int r;
+    
+    public double getRound(){
+        return 2*Math.PI*r;
+    }
+    public double getArea(){
+        return Math.PI*r*r;
+    }
+   
+   
+}
